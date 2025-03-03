412763c69107c0c51e2e9f29de7dac77537cbe49
akTARDIGRADE13 <itmtha@gmail.com>
2025-03-03 10:14:10 +0900

    feat: tailwindcssを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    integrations: [mdx()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
