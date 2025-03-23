ee2e4ee6afea640a208d7a948970955fd8ee6658
akTARDIGRADE13 <itmtha@gmail.com>
2025-03-23 22:40:30 +0900

    feat: Blogページの実装

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,5 @@
 // 1. `astro:content`からユーティリティをインポート
 import { z, defineCollection } from 'astro:content';
-import { Schema } from 'astro:schema';
 
 // 2. コレクションを定義
 const updatesCollection = defineCollection({ 
@@ -11,8 +10,20 @@ const updatesCollection = defineCollection({
     }),
 });
 
+const blogCollection = defineCollection({
+    schema: z.object({
+        id: z.string(),
+        title: z.string(),
+        date: z.string(),
+        updated: z.string(),
+        eyecatch: z.string(),
+        tags: z.array(z.string()),
+    }),
+});      
+
 // 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
 //    このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
 export const collections = {
     'updates': updatesCollection,
+    'blog': blogCollection,
 };
